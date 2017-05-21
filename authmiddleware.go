7fd57030f06f51d4f2c2e871df96fe722b3c38ab
hanjm <hanjinming@outlook.com>
2017-05-21 13:40:29 +0800

    update doc string

--- authmiddleware.go
+++ authmiddleware.go
@@ -7,8 +7,8 @@ import (
 // AuthFunc is your custom auth function type
 type AuthFunc func(ctx *fasthttp.RequestCtx) bool
 
-// NewAuthMiddleware accepts a customer auth function and then returns a middleware which
-// if auth function returns false, it will term the HTTP request and response 403 status code
+// NewAuthMiddleware accepts a customer auth function and then returns a middleware which only accepts auth passed request.
+// If auth function returns false, it will term the HTTP request and response 403 status code
 func NewAuthMiddleware(authFunc AuthFunc) Middleware {
 	return func(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 		return func(ctx *fasthttp.RequestCtx) {
