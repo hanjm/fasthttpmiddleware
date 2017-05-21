2be3cf5175c2a548e230a18a71ad4ce46b12382f
hanjm <hanjinming@outlook.com>
2017-05-21 11:29:17 +0800

    fix panic trace field name

--- recovermiddleware.go
+++ recovermiddleware.go
@@ -6,7 +6,7 @@ import (
 )
 
 // NewRecoverMiddleware return a middleware which can let app recover from a panic in request handler.
-// panic stack info will appear on the field named "trace" in the log line
+// panic stack info will appear on the field named "stacktrace" in the log line
 func NewRecoverMiddleware(logger *zap.Logger) Middleware {
 	return func(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 		return func(ctx *fasthttp.RequestCtx) {
