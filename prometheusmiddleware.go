1e88a5b0b74cf3bc9478808186791fa4ed91f662
hanjm <hanjm@live.com>
2017-05-22 18:14:26 +0800

    fix when to invoke NewPrometheusMiddleware twice lead a panic. remove log function in PrometheusMiddleware

--- prometheusmiddleware.go
+++ prometheusmiddleware.go
@@ -5,7 +5,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/valyala/fasthttp"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	"net/http"
 	_ "net/http/pprof"
 	"strconv"
@@ -28,37 +27,25 @@ var (
 
 // NewPrometheusMiddleware return a middleware which can be used by [prometheus](https://github.com/prometheus/prometheus) collecting metrics.
 // The prometheus is a monitoring system and time series database.
-// Note: the returned middleware contains the function of logmiddleware.
 func NewPrometheusMiddleware(bindAddr string, xRealIp bool, logger *zap.Logger) Middleware {
-	if !isRegistered {
-		go func() {
+	go func() {
+		if !isRegistered {
 			prometheus.MustRegister(requestCounter)
 			prometheus.MustRegister(responseTimeSummary)
 			isRegistered = true
 			http.Handle("/metrics", promhttp.Handler())
 			logger.Debug("prometheus metrics server start at " + bindAddr)
 			http.ListenAndServe(bindAddr, nil)
-		}()
-	}
+		}
+	}()
 	return func(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 		return func(ctx *fasthttp.RequestCtx) {
 			startTime := time.Now()
 			h(ctx)
-			var addrField zapcore.Field
-			if xRealIp {
-				addrField = zap.ByteString("addr", ctx.Request.Header.Peek("X-Real-IP"))
-			} else {
-				addrField = zap.String("addr", ctx.RemoteAddr().String())
-			}
 			promLabels := prometheus.Labels{"code": strconv.Itoa(ctx.Response.StatusCode()), "method": string(ctx.Method()), "path": string(ctx.Path())}
 			responseTime := time.Since(startTime).Seconds() * 1000
 			responseTimeSummary.With(promLabels).Observe(responseTime)
 			requestCounter.With(promLabels).Inc()
-			if ctx.Response.StatusCode() / 100 == 2 {
-				logger.Info("access", zap.Int("code", ctx.Response.StatusCode()), zap.Float64("time", responseTime), zap.String("method", promLabels["method"]), zap.String("path", promLabels["path"]), addrField)
-			} else {
-				logger.Warn("access", zap.Int("code", ctx.Response.StatusCode()), zap.Float64("time", responseTime), zap.String("method", promLabels["method"]), zap.String("path", promLabels["path"]), addrField)
-			}
 		}
 	}
 }
